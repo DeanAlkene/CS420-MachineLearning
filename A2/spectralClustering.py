49ee163e8305ad5f5abfb6d48b241e7a5ba3196c
DeanAlkene <deanlhz@sjtu.edu.cn>
2020-05-16 13:52:07 +0800

    Finished T4&T5

--- A2/spectralClustering.py
+++ A2/spectralClustering.py
@@ -26,14 +26,12 @@ def main():
     blobs = datasets.make_blobs(n_samples=n_samples, random_state=10)
     no_structure = np.random.rand(n_samples, 2), None
 
-    # Anisotropicly distributed data
     random_state = 160
     X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
     transformation = [[0.6, -0.6], [-0.4, 0.8]]
     X_aniso = np.dot(X, transformation)
     aniso = (X_aniso, y)
 
-    # blobs with varied variances
     varied = datasets.make_blobs(n_samples=n_samples,
                                 cluster_std=[1.0, 2.5, 0.5],
                                 random_state=random_state)
